81d5efeee84717c9ec5e02c2c34f9fc3409ca2d9
Zeno <zenocbda@gmail.com>
2020-10-21 00:19:11 +0800

    add basic structure

---
+++ algorithm/DynamicConnectivity.go
@@ -0,0 +1,20 @@
+package algorithm
+
+type UF []int
+
+func (uf *UF) Union() {
+
+}
+
+func (uf *UF) Connected() bool {
+
+	return false
+}
+
+func (uf *UF) Find() int {
+	return 0
+}
+
+func (uf *UF) Count() int {
+	return 0
+}
